bf4b59ad8b9a44c8f0bd49117860605803593779
gbleigh5 <gbleigh5@gmail.com>
2019-08-19 13:11:26 +0300

    modified the commands except for one

---
+++ dev-roadmap-master/README.md
@@ -0,0 +1,5 @@
+# Dev-roadmap
+A series of tasks based on the developer road map (https://github.com/kamranahmedse/developer-roadmap)
+
+# Tasks
+1. [Terminal Usage](terminal/README.md)
---
+++ dev-roadmap-master/terminal/starter.sh
@@ -0,0 +1,24 @@
+mkdir ~/terminal/www
+cd ~/terminal/www/
+
+:'echo "www folder is created"
+echo "As soon as you are reading this, 6 files and three directories are going to be created"
+touch .hidden.env
+touch index.html
+touch services.html
+touch services.old.html
+touch about.html
+touch about.old.html
+touch robot.txt
+touch debug.log
+touch error.log'
+
+
+# echo "The list of files created will be displayed"
+# ls -a
+
+:'FILE_PATH_NAME = "~/terminal/www"
+mkdir $FILE_NAME
+cd $FILE_PATH_NAME'
+:'mkdir ~/terminal/www
+cd ~/terminal/www/'
